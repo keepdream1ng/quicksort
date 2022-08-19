7f371ae71a29980ac6b350c6455e49ba292610bd
keepdream1ng <gotnickname@gmail.com>
2022-08-19 17:17:05 +0300

    it swaps ints!

--- quicksort.c
+++ quicksort.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h> //for atoi
+
+//next function swaps integers without temp
+void swap (int arr[], int i, int j) {
+        if (i!=j) {
+    arr[i]+=arr[j];
+    arr[j]=arr[i]-arr[j];
+    arr[i]-=arr[j];
+    }    
+} 
 
 int main (int argc, char **argv) {
     for (int i=0; i<argc; i++) {
         printf("arg %d - %s\n",i, argv[i]);
     }
+//making array with ints, not strings
+    int target[argc-1];
+    for (int i=1; i<argc; i++) {
+       target[i-1]=atoi(argv[i]); 
+    }
+//testing swap
+    swap (target,1,3);
+
+    for (int i=0; i<argc-1; i++) {
+        printf ("%d ", target[i]);
+    }
+    return 0;
 }
