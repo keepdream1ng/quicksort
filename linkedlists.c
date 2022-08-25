eaad4b011a07fb2487f9bb0c3a50d7ec854b3254
keepdream1ng <gotnickname@gmail.com>
2022-08-25 21:32:08 +0300

    added funcion delete_whole

--- linkedlists.c
+++ linkedlists.c
@@ -23,29 +23,44 @@ void printlist(node_t *head) {
 //function allocates memory, dont forget to free it!
 node_t* create_new_node(int val) {
     node_t* result= (node_t*) malloc(sizeof(node_t));
-    result->val = val;
-    result->next = NULL;
+    if (result!=NULL) {
+        result->val = val;
+        result->next = NULL;
+    }
     return result;
 }
     
+//funcion deletes whole list from head by freeing memory
+void delete_whole (node_t *head) {
+    if (head == NULL) {
+        printf("error");
+    } else {
+        do {
+            node_t *temporary = head;
+            head = head->next;
+            free(temporary);
+        } while (head!=NULL); 
+    }
+}
+
 
 int main() {
-    node_t n1, n2, n3;
-    node_t *head;
-
-    n1.val =45;
-    n2.val =8;
-    n3.val =32;
-    node_t *tmp = create_new_node(48);
+    node_t *head = create_new_node (45);
+    node_t *tmp1 = create_new_node(21);
+    node_t *tmp2 = create_new_node(68);
+    node_t *tmp3 = create_new_node(55);
 
 
-    head = &n3;
-    n3.next = &n2;
-    n2.next = tmp;
-    tmp->next=&n1;
-    n1.next = NULL;
 
-    printlist(head);
+    head->next = tmp3;
+    tmp3->next = tmp2;
+    tmp2->next = tmp1;
+    tmp1->next = NULL;
 
+    printlist (head);
+
+    delete_whole (head);
+
+    printlist (head);
     return 0;
 }
