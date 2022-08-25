91ce4d6395665b314456c9b7f71a6d9df30b9574
keepdream1ng <gotnickname@gmail.com>
2022-08-25 16:55:09 +0300

    added create_new_node function

--- linkedlists.c
+++ linkedlists.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+typedef struct node {
     int val;
     struct node* next;
-}; 
-typedef struct node node_t;
+} node_t; 
+
 
 void printlist(node_t *head) {
     if (head == NULL) {
@@ -14,11 +14,20 @@ void printlist(node_t *head) {
     } else {
         while (head!=NULL) {
             printf("%d-",head->val);
-            head = head->next;
+            head = head->next; //head is copied value, so its fine to change it
         }
         printf("\n");
     }
 }
+
+//function allocates memory, dont forget to free it!
+node_t* create_new_node(int val) {
+    node_t* result= (node_t*) malloc(sizeof(node_t));
+    result->val = val;
+    result->next = NULL;
+    return result;
+}
+    
 
 int main() {
     node_t n1, n2, n3;
@@ -27,10 +36,13 @@ int main() {
     n1.val =45;
     n2.val =8;
     n3.val =32;
+    node_t *tmp = create_new_node(48);
+
 
     head = &n3;
     n3.next = &n2;
-    n2.next = &n1;
+    n2.next = tmp;
+    tmp->next=&n1;
     n1.next = NULL;
 
     printlist(head);
