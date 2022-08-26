fb15f7ea226914182bf3d3b64f7f3078c276a418
keepdream1ng <gotnickname@gmail.com>
2022-08-26 10:05:38 +0300

    it takes ints from pipe now, just prints tho

--- testmain.c
+++ testmain.c
@@ -3,20 +3,40 @@
 #include "quicksort.h"
 #include "linkedlists.h"
 
+#define MAX_INPUT 100
+
 int main (int argc, char **argv) {
-    if (argc<=2) {
-        return 0;
-        //тут сделать linked lists из STDin которые, потом можно преобразовать в int массив
-    }
+    if (argc<=2) { //for the pipeline input
+        char input[MAX_INPUT];
+        scanf("%s", input);
+        if (input[0]=='q') return 0; // exit from programm screen
+        int count = 0;
+        node_t *head = create_new_node(atoi(input));
+        node_t *tmp = head; // temporary pointer for linked list creation
+        while (scanf("%s", input)!=EOF) {
+            tmp = insert_node_after( tmp, create_new_node ( atoi(input)));
+            count++;
+        }
+        printlist (head);
+//converting linked list to array for sorting
+/*
+        int target[count];
+        tmp=head;
+        for (int i=0; i<=count; i++) {
+            target[i] = tmp->val;
+            tmp = tmp->next;
+        }
+*/
+    } else { // for the terminal input which comes in *argc
 //making array with ints, not strings
-    int target[argc-1];
-    for (int i=1; i<argc; i++) {
-       target[i-1]=atoi(argv[i]); 
-    }
+        int target[argc-1];
+        for (int i=1; i<argc; i++) {
+            target[i-1]=atoi(argv[i]); 
+        }
 
     quicksort(target, 0, argc-2);
 
     printarr(target,0, argc-2);
-    
+}    
     return 0;
 }
