ca006825909b7dfe6c3c16f1c0e013bb23548cda
keepdream1ng <gotnickname@gmail.com>
2022-08-26 11:16:08 +0300

    it sometimes works as intended

--- testmain.c
+++ testmain.c
@@ -17,26 +17,25 @@ int main (int argc, char **argv) {
             tmp = insert_node_after( tmp, create_new_node ( atoi(input)));
             count++;
         }
-        printlist (head);
 //converting linked list to array for sorting
-/*
-        int target[count];
+        int *target_arr = (int *) malloc(count * sizeof(int));
         tmp=head;
         for (int i=0; i<=count; i++) {
-            target[i] = tmp->val;
+            target_arr[i] = tmp->val;
             tmp = tmp->next;
         }
-*/
+        delete_whole(head);
+        quicksort (target_arr, 0, count);
+        printarr (target_arr, 0, count);
+        free (target_arr);
     } else { // for the terminal input which comes in *argc
 //making array with ints, not strings
         int target[argc-1];
         for (int i=1; i<argc; i++) {
             target[i-1]=atoi(argv[i]); 
         }
-
-    quicksort(target, 0, argc-2);
-
-    printarr(target,0, argc-2);
-}    
+        quicksort(target, 0, argc-2);
+        printarr(target,0, argc-2);
+    }    
     return 0;
 }
